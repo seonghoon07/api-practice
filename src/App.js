7a3280b959d935c27a5a10430b6b297ff3aa825c
seonghoon07 <kkukkuli07@gmail.com>
2023-08-09 23:28:57 +0900

    API 불러오기 성공

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+// api 287b2a1c5e56ff945a281d282236b623
 function App() {
+  const API_ADDRESS = "287b2a1c5e56ff945a281d282236b623"
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=Busan&appid=${API_ADDRESS}`
+
+  axios.get(url).then(response => console.log(response))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='base'>
+      
     </div>
   );
 }
