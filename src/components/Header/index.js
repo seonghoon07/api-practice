d42ac0d5823fbd04a2924b1eb25ff43fa79d14c7
seonghoon07 <kkukkuli07@gmail.com>
2023-08-11 21:39:14 +0900

    button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,10 @@ import './style.css'
 const Header = () => {
   return (
     <header>
-      
+      <button>기온</button>
+      <button>시간별 예보</button>
+      <button>날짜별 예보</button>
+      <button>그 외</button>
     </header>
   );
 };
