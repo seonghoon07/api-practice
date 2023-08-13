ac12e4df2f32dd8610183c002a1c26c564771e7e
seonghoon07 <kkukkuli07@gmail.com>
2023-08-13 18:53:46 +0900

    Header-button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,12 @@ import './style.css'
 const Header = () => {
   return (
     <header>
-      <button>기온</button>
-      <button>시간별 예보</button>
-      <button>날짜별 예보</button>
-      <button>그 외</button>
+      <nav>
+        <button className='header_button'>기온</button>
+        <button className='header_button'>시간별 예보</button>
+        <button className='header_button'>날짜별 예보</button>
+        <button className='header_button'>그 외</button>
+      </nav>
     </header>
   );
 };
